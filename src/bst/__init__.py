c099c8565e5a0a895e4e768ff90e5a000b8062f4
Thomas <thomas.sturges@kit.edu>
2026-06-29 15:56:49 +0200

    Made imports in the init file more explicit so it plays nice with MkDocs

--- src/bst/__init__.py
+++ src/bst/__init__.py
@@ -1,1 +1,21 @@
-from .bst import *
+from .bst import (
+    BSTPlan,
+    bst,
+    generate_bst,
+    ibst,
+    generate_ibst,
+    bst2D,
+    generate_bst2D,
+    generate_ibst2D,
+)
+
+__all__ = [
+    "BSTPlan",
+    "bst",
+    "generate_bst",
+    "ibst",
+    "generate_ibst",
+    "bst2D",
+    "generate_bst2D",
+    "generate_ibst2D",
+]
